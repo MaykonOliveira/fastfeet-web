47c972865b6d4686512195b888744e9a56a485af
Maykon Oliveira <maykon.oliveira@neoway.com.br>
2020-02-28 10:14:34 -0300

    Configured init of the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <h1>Olá</h1>;
+  return (
+    <>
+      <h1>Olá</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
