4cd1e4414b14aaec19d1b157aebc33682584de42
MaykonOliveira <moliveira.anschau@gmail.com>
2020-03-08 07:33:10 -0300

    Add structure of the orders page

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import { MdSearch, MdAdd, MdMoreHoriz } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Search } from './styles';
 
 export default function Orders() {
   return (
     <Container>
-      <h1>Orders</h1>
+      <strong>Gerenciando encomendas</strong>
+
+      <div>
+        <Search>
+          <button type="button">
+            <MdSearch size={16} color="#A7A7A7" />
+          </button>
+          <input placeholder="Buscar por encomendas" />
+        </Search>
+        <button type="button">
+          <MdAdd size={16} color="#fff" />
+          <span>CADASTRAR</span>
+        </button>
+      </div>
+
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Destinatário</th>
+          <th>Entregador</th>
+          <th>Cidade</th>
+          <th>Estado</th>
+          <th>Status</th>
+          <th>Ações</th>
+        </tr>
+        <tr>
+          <td>#01</td>
+          <td>Ludwig van Beethoven</td>
+          <td>
+            <img
+              src="https://api.adorable.io/avatars/40/john.png"
+              alt="John Doe"
+            />
+            <span>John Doe</span>
+          </td>
+          <td>Rio do Sul</td>
+          <td>Santa Catarina</td>
+          <td>
+            <div>
+              <div />
+              <span>ENTREGUE</span>
+            </div>
+          </td>
+          <td>
+            <button type="button">
+              <MdMoreHoriz />
+            </button>
+          </td>
+        </tr>
+      </table>
     </Container>
   );
 }
--- src/pages/Orders/styles.js
+++ src/pages/Orders/styles.js
@@ -1,3 +1,69 @@
 import styled from 'styled-components';
+import { darken, lighten } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #444444;
+  margin: 30px 90px;
+
+  strong {
+    font-size: 20px;
+    margin-bottom: 30px;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+
+    ~ button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 5px 0 0;
+      height: 30px;
+      padding: 6px;
+      background: #7d40e7;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 12px;
+      transition: background 0.2s;
+      margin-bottom: 20px;
+      &:hover {
+        background: ${darken(0.08, '#7d40e7')};
+      }
+    }
+  }
+`;
+
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  background: #fff;
+  height: 30px;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #fff;
+    border: 0;
+    padding: 6px;
+    &:hover {
+      background: ${darken(0.02, '#fff')};
+    }
+  }
+
+  input {
+    border: 0;
+    padding: 5px 10px;
+    color: #444444;
+    &::placeholder {
+      color: ${lighten(0.3, '#444444')};
+    }
+  }
+`;
